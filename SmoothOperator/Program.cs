826e4a4344456330a8d3cfdf23750a6dc8b8c7f2
duducg <eduardo.sang.rocha@gmail.com>
2024-02-28 16:11:33 +0000

    Start Exercício 1 - SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt user for a non negative integer
+            Console.Write("Please insert non negative integer: ");
+            string user_in = Console.ReadLine();
+
+            //convert input to type "byte"
+            //bytes go from 0 >> 255 (total of 256)
+
+            byte n = byte.Parse(user_in);
+            Console.WriteLine(n);//test
+
+            
+        }
+    }
+}
