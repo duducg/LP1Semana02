f1c1687d604006075ec79974ac8858bf31e58801
duducg <eduardo.sang.rocha@gmail.com>
2024-02-28 16:43:12 +0000

    Added Exercise 4 operations

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -16,16 +16,28 @@ namespace SmoothOperator
             byte n = byte.Parse(user_in);       
 
             //Decrement
-            Console.WriteLine($"Input decremented by 1 : {--n}");
+            Console.WriteLine($"n decremented by 1 : {--n}");
 
             //Increment
-            Console.WriteLine($"Input incremented by 1 : {++n}");
+            Console.WriteLine($"n incremented by 1 : {++n}");
 
             //if input is 0 then the decrement will return 255
         
-            
+            //Exercise 4
 
+            //Divide by 2
+            Console.WriteLine($"n divided by 2 : {n/2}");
+
+            //Shift left by 3 <<
+            Console.WriteLine($"n shifted left by 3 : {n<<3}");
+
+            //XOR with 5
+            Console.WriteLine($"XOR with 5: {n ^ 5}");
             
+            //is greater then 10
+            Console.WriteLine($"Is n greater then 10? {n>10}");
+
+            //Note: next time skip the string interpolation
         }
     }
 }
