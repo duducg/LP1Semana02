e8401a1bdfa684c6183a9951669ab3d061061f7d
duducg <eduardo.sang.rocha@gmail.com>
2024-02-28 17:13:09 +0000

    Prompt user for input values

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,6 +7,19 @@ namespace Cilindro
         static void Main(string[] args)
         {
             //Exercise 5
+            Console.WriteLine("Cylinder Exercise:");
+            
+            //Prompt user for Cylinder params:
+            //Height
+            Console.Write("Please input height:");
+            string h = Console.ReadLine();
+
+            //Radius
+            Console.Write("Please input radius:");
+            string r = Console.ReadLine();
+
+                    
+
         }
     }
 }
