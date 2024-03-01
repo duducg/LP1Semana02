72e95764b5c1125b1c056e7bf1e90fb0e4c82841
duducg <eduardo.sang.rocha@gmail.com>
2024-03-01 17:33:39 +0000

    MagicGun done

--- MagicGun/Program.cs
+++ MagicGun/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace MagicGun
 {
@@ -6,7 +7,75 @@ namespace MagicGun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //string vars
+            string option,mf,ef,nf;
+            mf = "Magic Fire :/";
+            ef = "Electric Fire :/";
+            nf = "Normal fire :/";
+            
+            //Prompt user for a number
+            Console.Write("Please give me an integer number: ");
+            string user_in = Console.ReadLine();
+
+            //parse user input to int:
+            int n = int.Parse(user_in);
+            
+
+            //iterator value initialized to 1
+            int cn = 1;
+            
+            Console.WriteLine($"Number of shots: {n}");
+            //start loop here
+            while (cn < n)
+            {
+                //resets every loop cycle
+                option = "";
+                
+                //every 3 iterations:
+                if (cn % 3 ==0)
+                {
+                    //MAGIC FIRE!
+                    option = "magic";
+                }
+                //every 5 iterations:
+                if (cn % 5 ==0)
+                {
+                    //ELECTRIC FIRE!
+                    option = "electric";
+                }
+                //Both above conditions are true:
+                if (cn % 5 ==0 && cn % 3 ==0)
+                {
+                    //BOTH
+                    option = "both";
+                }
+                //default
+                string response = "";
+                switch (option)
+                {
+                    case "magic":
+                        response = mf;
+                        break;
+                    case "electric":
+                        response = ef;
+                        break;
+                    case "both":
+                        response = mf + " " + ef;
+                        break;
+                    default:
+                        response = nf;
+                        break;
+                }
+                
+                Console.WriteLine(cn +": " + response);
+
+
+                //increment
+                cn++;
+            }
+            
+
+            
         }
     }
 }
